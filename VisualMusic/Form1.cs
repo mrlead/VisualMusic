9935db7646b233cac1c38642b25fcca8be668018
Andrey Shevchenko <krendelek1509999@gmail.com>
2020-01-15 16:05:31 +0300

    Поправки работы с настройками

--- VisualMusic/Form1.cs
+++ VisualMusic/Form1.cs
@@ -6,6 +6,11 @@ namespace VisualMusic
 {
     public partial class mainForm : Form
     {
+        RegistryKey currentUserKey = Registry.CurrentUser;
+        public RegistryKey tableSet;
+
+        IniFiles INI = new IniFiles("data.ini");
+
         public int row, col = 0;
         public string path;
 
@@ -14,40 +19,64 @@ namespace VisualMusic
             InitializeComponent();
         }
 
-        //Конструктор, принимающий путь к INI-файлу
-        public INIManager(string aPath)
-        {
-            path = aPath;
-        }
-
         //Загрузка параметров таблицы из реестра системы
         private void mainForm_Load(object sender, System.EventArgs e)
         {
-            RegistryKey currentUserKey = Registry.CurrentUser;
-            RegistryKey tableSet = currentUserKey.OpenSubKey("VisualMusic");
+            try
+            {
+                tableSet = currentUserKey.OpenSubKey("VisualMusic");
+            }
+            catch
+            {
+                row = Convert.ToInt32(INI.ReadINI("table", "row"));
+                col = Convert.ToInt32(INI.ReadINI("table", "col"));
 
-            row = Convert.ToInt32(tableSet.GetValue("row").ToString());
-            col = Convert.ToInt32(tableSet.GetValue("col").ToString());
+                tableSet = currentUserKey.CreateSubKey("VisualMusic");
+                row = Convert.ToInt32(tableSet.GetValue("row").ToString());
+                col = Convert.ToInt32(tableSet.GetValue("col").ToString());
 
-            tableSet.Close();
+                tableSet.Close();
+            }
+            finally
+            {
+                row = Convert.ToInt32(tableSet.GetValue("row").ToString());
+                col = Convert.ToInt32(tableSet.GetValue("col").ToString());
+
+                tableSet.Close();
+            }
+        }
+
+        //Сохранение параметров таблицы при случайном закрытии программы 
+        private void mainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            try
+            {
+                tableSet = currentUserKey.CreateSubKey("VisualMusic");
+            }
+            catch
+            {
+                INI.Write("table", "row", Convert.ToString(row));
+                INI.Write("table", "col", Convert.ToString(col));
+
+                tableSet = currentUserKey.CreateSubKey("VisualMusic");
+                tableSet.SetValue("row", Convert.ToString(row));
+                tableSet.SetValue("col", Convert.ToString(col));
+
+                tableSet.Close();
+            }
+            finally
+            {
+                tableSet.SetValue("row", Convert.ToString(row));
+                tableSet.SetValue("col", Convert.ToString(col));
+
+                tableSet.Close();
+            }
         }
 
         private void butSettings_Click(object sender, EventArgs e)
         {
             settingsForm newForm = new settingsForm();
             newForm.Show();
-        }
-
-        //Сохранение параметров таблицы при случайном закрытии программы 
-        private void mainForm_FormClosing(object sender, FormClosingEventArgs e)
-        {
-            RegistryKey currentUserKey = Registry.CurrentUser;
-            RegistryKey tableSet = currentUserKey.CreateSubKey("VisualMusic");
-
-            tableSet.SetValue("row", Convert.ToString(row));
-            tableSet.SetValue("col", Convert.ToString(col));
-
-            tableSet.Close();
         }
     }
 
